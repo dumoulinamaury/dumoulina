1aea775825184c9a489d4c0ec5d12803d639eb5d
d.amaury <d.amaury@hotmail.com>
2017-10-12 20:38:13 +0200

    first commit - basic files and folders

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php wp_footer(); ?>
+/**
+ * Created by PhpStorm.
+ * User: Jema
+ * Date: 12/10/2017
+ * Time: 7:41 PM
+ */
---
+++ header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jema
+ * Date: 12/10/2017
+ * Time: 7:41 PM
+ */
+<?php
+
+wp_head();
